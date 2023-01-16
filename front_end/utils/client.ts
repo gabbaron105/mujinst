fb9c6f47e1192dab6c94ee5ede201c685de6ffcb
GB <112358594+gabbaron105@users.noreply.github.com>
2023-01-16 19:05:30 +0100

    Delete client.ts

--- front_end/utils/client.ts
+++
@@ -1,9 +0,0 @@
-const sanityClient = require('@sanity/client')
-
-export const client = sanityClient({
-  projectId: 't5wbn8ep',
-  dataset: 'production',
-  apiVersion: '2022-03-10',
-  useCdn: false,
-  token: 'skl6WAsz7FxgWywXbzRZgNydG6cpHd5PRZpUYeXFQ5JZz16EJQE56GkCbjRr90lhgPfbG4f3eNJjyfs5i6F7M1mOoktpKBECr8TMrTahL3q1o0pZcIm6vqwFnt27dqKJTxq28mfu4Udnw5lXCsDWUmnZ8uw8mLmZDFAQ5iasWi3DXSbMKaNz'
-});
